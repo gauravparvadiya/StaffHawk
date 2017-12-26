76aca19bac07a353ba6c66d20583d898597c9a9a
Gaurav Parvadiya <gaurav.parvadiya@gmail.com>
2017-12-26 17:50:53 -0500

    changes in root url patterns

--- StaffHawk/urls.py
+++ StaffHawk/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'^', include('Authentication.urls')),
-    url(r'^administrator/dashboard/', include('administrator.urls', namespace='administrator')),
+    url(r'^administrator/', include('administrator.urls', namespace='administrator')),
+    url(r'^bde/', include('bde.urls', namespace='bde')),
 ]
